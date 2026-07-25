8c1e2b34191476b19efa3a47a595dc5ea810d70f
YoshithaPasumarthi <yoshithapasumarthi@gmail.com>
2026-07-25 08:51:26 +0530

    project v2.0

---
+++ src/main/java/com/klef/soa/controller/PatientController.java
@@ -0,0 +1,41 @@
+package com.klef.soa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.klef.soa.entity.Patient;
+import com.klef.soa.service.PatientService;
+
+@RestController
+public class PatientController 
+{
+	@Autowired
+	 private PatientService service; 
+	
+	@GetMapping("/")
+	public String test()
+	{
+		return "SOA Programming and Microservices";
+	}
+	
+	@GetMapping ("/displayall")
+	public ResponseEntity<List<Patient>> displayallpatients()
+	{
+		List<Patient> patients = service.displayAllPatients();
+		return ResponseEntity.status(200).body(patients);
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<Patient> addpatient( @RequestBody Patient p)
+	{
+		Patient patient = service.addPatient(p);
+		return ResponseEntity.status(201).body(patient);
+	}
+			
+}
